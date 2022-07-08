839f1f558170dadd55576bbffc6ef02334a38ff5
LuceroCorona <lucerocgriglio@gmail.com>
2022-07-08 16:15:31 -0300

    carrito

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer'
+
+
+function App() {
+  const title="Simple Makeup";
+
+  const data = {
+    greeting: "(╯°□°）╯︵ ┻━┻"
+  };
+
+  return(
+    <>
+    <NavBar/>
+    <ItemListContainer greeting={data.greeting}/>
+    </> 
+  );
+}
+
+export default App;
